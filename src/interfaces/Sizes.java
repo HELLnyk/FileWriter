711fdf8c55fc68df1bc61970747447371700655e
hellnyk <yurock.ua@gmail.com>
2016-02-27 20:02:42 +0200

    Constants for elements, which is used in creating of widow and path to file name

---
+++ src/interfaces/Sizes.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public interface Sizes {
+
+    int BUTTON_HEIGHT = 20;
+    int BUTTON_WIDTH = 70;
+    int FILE_MANAGER_WINDOW_HEIGHT = 600;
+    int FILE_MANAGER_WINDOW_WIDTH = 400;
+    int MAIN_WINDOW_HEIGHT = 500;
+    int MAIN_WINDOW_WIDTH = 200;
+
+    String SEPARATOR_FOR_LINUX = "/";
+    String FIRST_ROOT_DIRECTORY_FOR_LINUX = "/";
+    String SEPARATOR_FOR_WINDOWS = "\\";
+    String FIRST_ROOT_DIRECTORY_FOR_WINDOWS = "D:\\";
+}
